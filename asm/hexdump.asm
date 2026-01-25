09778fc2c03493059289dfec4c6e31879b55746e
Scott Baker <smbaker@gmail.com>
2026-01-25 08:33:18 -0800

    move macros and functions to include files

--- asm/hexdump.asm
+++ asm/hexdump.asm
@@ -1,4 +1,4 @@
-; Parameter Passing Experiment
+; Hexdump Tool
 ; Scott Baker, https://www.smbaker.com/
 
 EXTRN		OpenByteStream:FAR, ReadByte:FAR, ReadBytes:FAR, WriteByte:FAR, WriteBsRecord:FAR, ErrorExit:FAR, Exit:FAR
@@ -24,37 +24,7 @@ sBuffer		EQU	1024
 Main		SEGMENT
 ASSUME		CS: Main
 
-%*DEFINE (PrintString(strOffset,strLen))(
-		push	ax
-		push	bx
-		lea	ax, %strOffset
-		mov	bx, %strLen
-		call	doPrintString
-		pop	bx
-		pop	ax
-)
-
-%*DEFINE (PrintChar(theChar))(
-		push	ax
-		mov	ax, %theChar
-		call	doPrintChar
-		pop	ax
-)
-
-%*DEFINE (PrintHex(theValue))(
-		push	ax
-		mov	ax, %theValue
-		call	doPrintHex
-		pop	ax
-)
-
-%*DEFINE (CheckError(theRegister)) LOCAL noError (
-		and	%theRegister,%theRegister
-		je	%noError
-		push	%theRegister
-		call	ErrorExit
-%noError:
-)
+$INCLUDE(macros.inc)
 
 Begin:		mov 	ax, Dgroup
 		mov 	SS, AX
@@ -119,209 +89,15 @@ moreColumns:	jmp	readLoop
 eof:
 		call	Exit
 
-openFile	PROC	NEAR
-		push	ds			; Arg 1: address pf Byte Stream Work Area
-		lea	ax, bswa
-		push	ax
-
-		push	paramSeg		; Arg 2: address and size of file spec
-		push	paramOfs
-		push	paramLen
-
-		push 	ds			; Arg 3: address and size of password
-		lea	ax, password
-		push	ax
-		mov	ax, 0			; length of password
-		push	ax
-
-		mov	ax, 06D72h		; Arg 4: mode. "mr" encoded as an integer.
-		push	ax
-
-		push	ds			; Arg 5: address and size of buffer
-		lea	ax, buffer
-		push	ax
-		mov	ax, sBuffer
-		push	ax
-
-		call	OpenByteStream
-		%CheckError(ax)
-		ret
-openFile	ENDP
-
-readFileByte	PROC	NEAR
-		push	ax
-		push	bx
-		push	cx
-		push	dx
-		push	si
-		push	di
-
-		push	ds			; Arg 1: address pf Byte Stream Work Area
-		lea	ax, bswa
-		push	ax
-
-		push	ds			; Arg 2: address of Byte
-		lea	ax, bReadRet
-		push	ax
-
-		call	ReadByte
-		cmp	ax, 1
-		jne	notEof
-		jmp	eof
-notEof:		%CheckError(ax)
-
-		pop	di
-		pop	si
-		pop	dx
-		pop	cx
-		pop	bx
-		pop	ax
-
-		ret
-readBuffer	ENDP
-
-		; doPrintstring
-		;    ax contains offset of string
-		;    bx contains length of string
-
-doPrintString	PROC	NEAR
-		push	cx
-		push	dx
-		push	si
-		push	di
-		push	ds			; push address of bsVid
-		lea	cx, bsVid
-		push	cx
-		push	ds			; push address of string
-		push	ax
-		push	bx			; push length of string
-		push 	ds			; push address of cbWrittenRet
-		lea	ax, cbWrittenRet
-		push	ax
-		call	WriteBsRecord
-		%CheckError(ax)
-		pop	di
-		pop	si
-		pop	dx
-		pop	cx
-		ret
-doPrintString	ENDP
-
-		; printbyte
-		;       ax contains byte
-
-doPrintChar	PROC	NEAR
-		push	bx
-		push	cx
-		push	dx
-		push	si
-		push	di
-		push	ds
-		lea	cx, bsVid
-		push	cx
-		push	ax
-		call	WriteByte
-		%CheckError(ax)
-		pop 	di
-		pop	si
-		pop	dx
-		pop	cx
-		pop	bx
-		ret
-doPrintChar	ENDP
-
-		; printHex
-		;       print hex value in AX
-
-doPrinthex      PROC    NEAR
-		push	bx
-		push    cx
-		push	dx
-		push	si
-		push	di
-
-                mov     cx,4
-print1:         push    cx
-                mov     cl, 4
-                rol     ax, cl
-                push    ax
-
-                mov     bx,ax
-                and     bl,0FH
-                add     bl,'0'
-                cmp     bl,'9'
-                jbe     print2
-                add     bl,'A'-'0'-10
-print2:
-                push    ds
-                lea     ax,bsVid
-                push    ax
-                push    bx
-                call    writeByte
-		%CheckError(ax)
-
-                pop     ax
-                pop     cx
-                loop    print1
-
-		pop	di
-		pop	si
-		pop	dx
-		pop	cx
-		pop	bx
-                ret
-doPrinthex      ENDP
-
-		; printHex2
-		;       print hex value in AL
-
-doPrinthex2     PROC    NEAR
-		push	bx
-		push    cx
-		push	dx
-		push	si
-		push	di
-
-		mov	ah, al
-
-                mov     cx,2
-print21:        push    cx
-                mov     cl, 4
-                rol     ax, cl
-                push    ax
-
-                mov     bx,ax
-                and     bl,0FH
-                add     bl,'0'
-                cmp     bl,'9'
-                jbe     print22
-                add     bl,'A'-'0'-10
-print22:
-                push    ds
-                lea     ax,bsVid
-                push    ax
-                push    bx
-                call    writeByte
-		%CheckError(ax)
-
-                pop     ax
-                pop     cx
-                loop    print21
-
-		pop	di
-		pop	si
-		pop	dx
-		pop	cx
-		pop	bx
-                ret
-doPrinthex2     ENDP
-
 error:		push	ax
 		call	ErrorExit
 
 insufficientParams:
 		%PrintString(strInsuff,lenInsuff)
 		call	Exit
+
+$INCLUDE(file.inc)
+$INCLUDE(print.inc)
 
 Main		ENDS
 
